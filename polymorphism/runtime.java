5bd7ab92d490aa802b752193e2fa2a4408d821f3
Manoj R gowda <160026926+Manojrgowdaa@users.noreply.github.com>
2024-08-01 08:49:13 +0530

    Add files via upload

---
+++ polymorphism/runtime.java
@@ -0,0 +1,30 @@
+class Employee {
+    void work(){
+        System.out.println("Working");
+    }
+}
+
+class Developer extends Employee {
+    @Override
+  public void work(){
+        System.out.println("Develop App");
+    }
+}
+
+class Tester extends Employee{
+    @Override
+    public void work(){
+        System.out.println("Testing App");
+    }
+}
+
+public class runtime{
+    public static void main(String[] args) {
+        Employee e = new Developer();
+                 e.work();
+
+       Employee e1 = new Tester();
+                e1.work();
+
+    }
+}
