4d6700bf7fa78e778338cae2f6366d0631f4f0a4
Manoj R gowda <160026926+Manojrgowdaa@users.noreply.github.com>
2024-08-02 22:37:32 +0530

    Add files via upload

---
+++ javaLibraries/Hashcode.java
@@ -0,0 +1,9 @@
+public class Hashcode {
+	public static void main(String[] args) {
+		Hashcode h = new Hashcode();
+		System.out.println(h.hashCode()); //Returns a hash code value for the object.
+	}
+ public int hashCode() { //Override
+	 return 234;
+ }
+}
---
+++ javaLibraries/StringEquals.java
@@ -0,0 +1,19 @@
+
+
+public class StringEquals {
+	String brand = "swift";
+	public static void main(String[] args) {
+		StringEquals se = new StringEquals();
+		StringEquals se1 = new StringEquals();
+		
+		System.out.println(se.equals(se1));
+		
+	
+	}
+	public boolean equals(Object ob) {
+		StringEquals o=(StringEquals)ob;   //dwncasting and 
+		System.out.println(o.brand);
+		return brand.equals(o.brand);
+	}
+}
+	
